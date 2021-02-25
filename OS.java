c44752a07d76a76f59c99b7c4b079759fbf4053b
Paul-Ryan-UAH <79427626+Paul-Ryan-UAH@users.noreply.github.com>
2021-02-24 18:27:26 -0600

    Update OS.java

--- OS.java
+++ OS.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class OS {
-    public static void main(String[] args){
-        Vector<Process> processTable = new Vector<Process>();
+    public void storeProcesses(){ // store all processes
+        Vector<Process> processTable = new Vector<Process>(); // initialize vector of processes
         try {
-            File dataFile = new File("filename.txt");
-            Scanner dataReader = new Scanner(dataFile);
-            while (dataReader.hasNextLine()) {
+            File dataFile = new File("filename.txt"); // initialize file object
+            Scanner dataReader = new Scanner(dataFile); // initialize reader
+            while (dataReader.hasNextLine()) { // iterates through each line and contructs a process structure for each
                 String data = dataReader.nextLine();
                 String[] line = data.split(",", 0);
                 processTable.add(new Process(line[0], line[1], line[2], line[3]));
                 System.out.println(data);
             }
             dataReader.close();
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) { // notifies the user if the file can't be found
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
     }
 }
