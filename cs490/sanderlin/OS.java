e687525fc30570105843bbc1f77f16f449903900
Paul-Ryan-UAH <psr0007@uah.edu>
2021-03-13 23:37:03 -0600

    Algorithm for running processes. Haven't figured out how to test or debug yet. Just putting this out there now so anyone can see how I'm approaching this and tell me if and where I'm going wrong or get inspiration for a better approach.

---
+++ cs490/sanderlin/OS.java
@@ -0,0 +1,71 @@
+package cs490.sanderlin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class OS {
+    private Vector<Process> processTable;
+
+    public void storeProcesses(){ // store all processes
+        processTable = new Vector<Process>(); // initialize vector of processes
+        try {
+            File dataFile = new File("filename.txt"); // initialize file object
+            Scanner dataReader = new Scanner(dataFile); // initialize reader
+            while (dataReader.hasNextLine()) { // iterates through each line and constructs a process structure for each
+                String data = dataReader.nextLine();
+                String[] line = data.split(",", 0);
+                processTable.add(new Process(line[0], line[1], line[2], line[3]));
+                System.out.println(data);
+            }
+            dataReader.close();
+        } catch (FileNotFoundException e) { // notifies the user if the file can't be found
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+    }
+
+    public void runProcesses(int timeUnit){
+        int time = 0;
+        int timeRemaining = 0;
+        int selection = 0;
+        boolean end = false;
+        boolean processing = false;
+         int status[] = new int[processTable.size()];
+         for(int i = 0; i < processTable.size(); i++){
+             status[i] = 0;
+         }
+         while(!end){
+             if(!processing){
+                 for(int i = 0; i < processTable.size(); i++){
+                     if(time >= processTable.get(i).getArrivalTime()){
+                         selection = i;
+                         timeRemaining = processTable.get(i).getServiceTime();
+                         break;
+                     }
+                 }
+                 processing = true;
+             }
+             else{
+                 timeRemaining = timeRemaining - timeUnit;
+                 if(timeRemaining <= 0) {
+                     status[selection] = 1;
+                     processing = false;
+                 }
+             }
+             time = time + timeUnit;
+             //update GUI
+             end = true;
+             for(int i = 0; i < processTable.size(); i++){
+                 if(status[i] == 0)
+                     end = false;
+             }
+         }
+    }
+
+    public static void main(String[]args){
+        OS test = new OS();
+        test.storeProcesses();
+    }
+}
